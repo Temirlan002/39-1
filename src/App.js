86308d029d238d6f21925134eed986a660cc8bbb
Tima19 <temirlan.baltabaev6@gmail.com>
2024-08-31 18:41:37 +0600

    lesson1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import ChangeTitle from './components/ChangeTitle';
+import ContactsPage from './contactsPage/ContactsPage';
+import AboutPage from './pages/aboutPage/AboutPage';
+import MainPage from './pages/mainPage/MainPage';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      {/* <MainPage /> */}
+      {/* <AboutPage /> */}
+      {/* <ChangeTitle /> */}
+      <ContactsPage />
+    </div>
+  );
+}
+
+export default App;
